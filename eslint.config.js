b64179920a1585c0601aa6ef5087c0932339c0a6
echo <play3step@naver.com>
2025-08-22 12:00:56 +0900

    [Chore] eslint 규칙 추가

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -13,11 +14,22 @@ export default tseslint.config([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite
+      reactRefresh.configs.vite,
+      prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser
+    },
+    rules: {
+      'react-hooks/exhaustive-deps': 'warn', // useEffect 의존성 검사 강화
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' }
+      ], // 안 쓰는 변수 체크
+      'no-console': ['warn', { allow: ['warn', 'error'] }], // console.log 방지
+      eqeqeq: ['error', 'always'], // 항상 === 사용
+      'no-debugger': 'error' // debugger 사용 금지
     }
   }
 ])

b90991b9b5c9dcf20ad169c347dfac9e4f6fb7e3
skyunlee98 <skyunlee@naver.com>
2025-08-24 23:09:41 +0900

    [Feat] Nav 컴포넌트 구현

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router'
+import Nav from '../nav'
 
 export const Layout = () => {
   return (
     <div className="min-h-dvh bg-zinc-100">
       <div className="mx-auto w-full max-w-[420px] min-h-dvh bg-white pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] overscroll-y-contain">
-        <main className="p-4">
+        <main className="p-4 pb-[60px]">
           <Outlet />
         </main>
+
+        <Nav />
       </div>
     </div>
   )
---
+++ src/shared/components/nav/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as HomeIcon } from './HomeIcon'
+export { default as BookIcon } from './BookIcon'
+export { default as VoteIcon } from './VoteIcon'
+export { default as MoreIcon } from './MoreIcon'

6c95e002b8d4d56cfd44c9a64b8c4b1c1726c1ed
echo <play3step@naver.com>
2025-08-26 20:43:39 +0900

    [Feat] 월별 수입/지출/잔액 표시

--- src/pages/calendar/ui/page.tsx
+++ src/pages/calendar/ui/page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useLoaderData } from 'react-router'
 import { TotalReport } from './TotalReport'
 import {
@@ -9,6 +9,7 @@ import {
   useCalendar
 } from '@/features/calendar/index'
 import type { AccountItem } from '@/features/accountItem/index'
+import { useShallow } from 'zustand/shallow'
 
 interface LoaderData {
   events: AccountItem[]
@@ -19,12 +20,30 @@ export const CalendarPage = () => {
   const [isOpen, setIsOpen] = useState(false)
   const { initialDate, events } = useLoaderData() as LoaderData
 
-  const setData = useSelectedDate(s => s.setDate)
+  const [setData, setAmountList] = useSelectedDate(
+    useShallow(s => [s.setDate, s.setAmountList])
+  )
+
   const { calendarEventsByDate, getCalendarByDate } = useCalendar()
+
+  const calc = (events: AccountItem[]) =>
+    events.reduce(
+      (a, c) => {
+        if (c.type === 'income') {
+          a.income += c.amount
+        } else {
+          a.expense += c.amount
+        }
+        a.balance = a.income - a.expense
+        return a
+      },
+      { income: 0, expense: 0, balance: 0 }
+    )
 
   useEffect(() => {
     setData(new Date(initialDate))
-  }, [initialDate, setData])
+    setAmountList(calc(events))
+  }, [initialDate, setData, setAmountList, events])
 
   return (
     <div className="flex flex-col gap-2 items-center h-full">

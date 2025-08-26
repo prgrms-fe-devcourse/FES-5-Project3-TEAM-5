928682e3a04202e8fce09de53b1d2650303b9187
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-08-26 09:34:23 +0900

    [Feat]: 투표 조회 - 투표 선택바 애니메이션 추가

--- src/features/vote/ui/Selection.tsx
+++ src/features/vote/ui/Selection.tsx
@@ -1,21 +1,33 @@
 import inactiveCharacter from '@/shared/assets/vote/inactiveCharacter.png'
 import activeCharacter from '@/shared/assets/vote/activeCharacter.png'
+import { motion } from 'framer-motion'
 
-function Selection() {
+interface Props {
+  isVote: boolean
+  selectionText: string
+  percent?: number
+}
+
+function Selection({ isVote, percent = 0, selectionText }: Props) {
   return (
     <>
       <div>
         <div className="flex items-center gap-x-2 mb-1">
           <img
             className="w-8"
-            src={inactiveCharacter}
+            src={isVote ? activeCharacter : inactiveCharacter}
             alt="선택 아이콘"
           />
-          <p className="flex-1">먹는다</p>
-          <p>34%</p>
+          <p className="flex-1">{selectionText}</p>
+          <p>{percent}%</p>
         </div>
         <div className="w-full h-2 bg-neutral-200 rounded-md">
-          <div className="w-60 h-2 bg-primary-light rounded-md"></div>
+          <motion.div
+            className="h-2 bg-primary-light rounded-md"
+            initial={{ width: '0%' }}
+            animate={{ width: `${percent}%` }}
+            transition={{ duration: 0.7, ease: 'easeOut' }}
+          />
         </div>
       </div>
     </>

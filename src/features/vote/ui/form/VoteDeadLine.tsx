b159a167c1d51cb1999388661782b849dd42561c
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-08-26 14:01:38 +0900

    [Feat]: 투표 작성 -  컴포넌트 추가

---
+++ src/features/vote/ui/form/VoteDeadLine.tsx
@@ -0,0 +1,13 @@
+import TimeButtonList from './TimeButtonList'
+
+function VoteDeadLine() {
+  return (
+    <div>
+      <h3 className="text-size-lg font-bold text-neutral-dark mb-3">
+        투표 종료 시간
+      </h3>
+      <TimeButtonList />
+    </div>
+  )
+}
+export default VoteDeadLine
---
+++ src/features/vote/ui/form/VoteQuestion.tsx
@@ -0,0 +1,16 @@
+function VoteQuestion() {
+  return (
+    <div>
+      <h3 className="text-size-lg font-bold text-neutral-dark mb-3">
+        투표 질문
+      </h3>
+      <div className="items-start rounded-lg border-2 p-4">
+        <textarea
+          className="w-full text-black resize-none focus:outline-none"
+          placeholder="질문을 입력해 주세요"></textarea>
+        <p className="text-neutral-dark ">입력수/50</p>
+      </div>
+    </div>
+  )
+}
+export default VoteQuestion

08a4cd5c2d2fc22d547cb3f693ad502514bc2868
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-08-27 17:56:59 +0900

    feat: 투표 조회 - 투표 마감 전 후 ui 변경

--- src/features/vote/ui/VoteCard.tsx
+++ src/features/vote/ui/VoteCard.tsx
@@ -1,43 +1,67 @@
 import { Link } from 'react-router'
 import { ResultOption } from './ResultOption'
 import type { VoteOptions } from '../model/type'
-import { formatDate, getDeadline } from '../utils/Date'
+import { formatDate } from '../utils/Date'
+import { tw } from '@/shared/utils/tw'
 
 interface Props {
-  people: number
-  vote_id: string
+  voteId: string
   question: string
-  starts_at: string
+  startsAt: string
   deadline: string
   isMine: boolean
-  vote_options: VoteOptions[]
+  isActive: boolean
+  voteOptions: VoteOptions[]
+  participants: number
   onDelete: () => void
 }
 
 export function VoteCard({
-  people,
   question,
-  vote_options,
+  voteOptions,
   deadline,
-  starts_at,
+  startsAt,
   isMine,
-  vote_id,
+  voteId,
+  isActive,
+  participants,
   onDelete
 }: Props) {
-  const startedDate = formatDate(starts_at)
+  const startedDate = formatDate(startsAt)
+  const divClassName = () => {
+    switch (isActive) {
+      case true:
+        return 'border-primary-light '
+      case false:
+        return 'bg-neutral-light border-neutral-DEFAULT '
+      default:
+        break
+    }
+  }
+  const deadlineClassName = () => {
+    if (isActive) {
+      return 'text-black'
+    } else {
+      return 'text-neutral-dark '
+    }
+  }
 
   return (
-    <div className="flex flex-col w-full p-4 gap-4 rounded-lg border-2 border-primary-light ">
+    <div
+      className={tw(
+        'flex flex-col w-full p-4 gap-4 rounded-lg border-2 ',
+        divClassName()
+      )}>
       <div className="flex justify-between text-neutral-dark text-size-md">
         <p>
           {startedDate}
-          <span className="text-black font-bold ml-3 ">
-            종료 {deadline} 전!
+          <span className={tw(' font-bold ml-3', deadlineClassName())}>
+            {deadline}
           </span>
         </p>
         {isMine && (
           <div className="flex gap-2">
-            <Link to={`/vote/edit/${vote_id}`}>
+            <Link to={`/vote/edit/${voteId}`}>
               <button className="hover:font-bold  cursor-pointer">수정</button>
             </Link>
             <button
@@ -48,23 +72,21 @@ export function VoteCard({
           </div>
         )}
       </div>
-
       <div
         className="bg-neutral-light p-3 rounded-lg border-2 border-neutral-200 text-black break-keep
 wrap-break-word">
         <p>{question}</p>
       </div>
-
-      {vote_options &&
-        vote_options.map(({ content, id }) => (
+      {voteOptions &&
+        voteOptions.map(({ content, id }) => (
           <ResultOption
             key={id}
-            percent={100}
+            voteOptions={voteOptions}
+            participants={participants}
             selectionText={content}
           />
         ))}
-
-      <p className="text-size-md text-neutral-dark">{people}명 참여</p>
+      <p className="text-size-md text-neutral-dark">{participants}명 참여</p>
     </div>
   )
 }

ca72d96cedeb48549f4feb8ea63ac5050d38d535
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-08-27 14:43:32 +0900

    feat: 투표 조회 - 정렬 버튼 기능 추가

--- src/features/vote/ui/filter/SortButtonList.tsx
+++ src/features/vote/ui/filter/SortButtonList.tsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
 import ButtonLayout from '../form/ButtonLayout'
+import type { Vote } from '../../model/type'
+import { filterDeadline, filterHotVote, filterMyVote } from '../../utils/filter'
 
-export function SortButtonList() {
+interface Props {
+  voteList: Vote[]
+  setFilteredList: (list: Vote[]) => void
+}
+
+export function SortButtonList({ voteList, setFilteredList }: Props) {
   const [sortButtonData, setSortButtonData] = useState([
     { id: 1, text: '전체', status: 'active' },
     { id: 2, text: '내 투표', status: 'inactive' },
@@ -9,12 +16,32 @@ export function SortButtonList() {
     { id: 4, text: '종료임박', status: 'inactive' }
   ])
 
+  const onChangeVoteList = (clickedId: number) => {
+    switch (clickedId) {
+      case 1:
+        setFilteredList(voteList)
+        break
+      case 2:
+        setFilteredList(filterMyVote(voteList))
+        break
+      case 3:
+        setFilteredList(filterHotVote(voteList))
+        break
+      case 4:
+        setFilteredList(filterDeadline(voteList))
+        break
+      default:
+        break
+    }
+  }
+
   const onStatusChange = (clickedId: number) => {
     const updated = sortButtonData.map(item =>
       item.id === clickedId
         ? { ...item, status: 'active' }
         : { ...item, status: 'inactive' }
     )
+    onChangeVoteList(clickedId)
     setSortButtonData(updated)
   }
 

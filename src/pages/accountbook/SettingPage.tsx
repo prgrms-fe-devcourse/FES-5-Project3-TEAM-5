3cffa959440959e0f081eb433e970ca5b37fcea3
EunbinJung <ainatube39@gmail.com>
2025-09-01 17:32:58 +0900

    fix/ 빌드에러

--- src/pages/accountbook/SettingPage.tsx
+++ src/pages/accountbook/SettingPage.tsx
@@ -1,6 +1,5 @@
 import AccountBookCard from '@/features/accountbook/setting/ui/AccountBookCard'
 import ServiceCard from '@/features/accountbook/setting/ui/ServiceCard'
-import EditGroup from '@/features/group/edit/EditGroup'
 
 function SettingPage() {
   return (

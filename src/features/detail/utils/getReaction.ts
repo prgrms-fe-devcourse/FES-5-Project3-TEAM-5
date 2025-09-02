50838c5f17b3029e9d6a0849ac55ec6e34f81e52
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-02 22:37:33 +0900

    refactor: 가계부 상세 아이템 조회 -  getReaction.ts 리액션 데이터 없을때 예외처리 추가

--- src/features/detail/utils/getReaction.ts
+++ src/features/detail/utils/getReaction.ts
@@ -2,6 +2,7 @@ import type { Reactions } from '../model/responseBody'
 
 // 좋아요, 싫어요 버튼 수 카운트
 export function getReactionCount(reactions: Reactions[]) {
+  if (!reactions) return { dislike: 0, like: 0 }
   const dislike = reactions.filter(item => item.kind === 'dislike').length
   const like = reactions.filter(item => item.kind === 'like').length
   return { dislike, like }
@@ -9,6 +10,7 @@ export function getReactionCount(reactions: Reactions[]) {
 
 // 유저가 클릭한 버튼 반환
 export function getUserReaction(reactions: Reactions[], userId: string) {
+  if (reactions.length === 0) return { kind: 'none' }
   const userReaction = reactions.filter(item => item.user_id === userId)
   return userReaction[0]
 }

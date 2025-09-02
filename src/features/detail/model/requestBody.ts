ec818c6fc9738e4d90a1867f0228f5d87b465f8c
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-02 22:09:04 +0900

    api: 가계부 상세 아이템 조회 - 댓글 추가 코드

---
+++ src/features/detail/model/requestBody.ts
@@ -0,0 +1,11 @@
+export interface ReactionTable {
+  item_id: string
+  user_id: string
+  kind: string
+}
+
+export interface CommentTable {
+  item_id: string
+  user_id: string
+  content: string
+}

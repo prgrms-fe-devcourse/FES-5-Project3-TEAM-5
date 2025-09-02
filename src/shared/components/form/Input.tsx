eb218ab9e7515e02dd5416be4284ed9e38450579
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-02 15:29:50 +0900

    refactor: 공통 input - placeholder 문구 수정

--- src/shared/components/form/Input.tsx
+++ src/shared/components/form/Input.tsx
@@ -30,7 +30,7 @@ const Input = forwardRef<HTMLInputElement, Props>(
           'bg-white rounded-lg border-2 border-neutral-light w-full py-2 px-1 text-black focus:border-primary-light focus:outline focus:outline-primary-light',
           className
         )}
-        placeholder={`${label}를 입력해 주세요`}
+        placeholder={`${label} 입력해 주세요`}
       />
     )
   }

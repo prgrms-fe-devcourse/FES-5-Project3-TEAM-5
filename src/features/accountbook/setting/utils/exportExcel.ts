1ca3b7c38733f9360cd1e037828afa0460ca2921
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-03 17:45:43 +0900

    fix: any type

--- src/features/accountbook/setting/utils/exportExcel.ts
+++ src/features/accountbook/setting/utils/exportExcel.ts
@@ -70,9 +70,9 @@ export function downloadExcel(
 
   // 안전한 UA 체크
   const ua = navigator.userAgent || ''
-  // 'eslint-disable-next-line @typescript-eslint/no-explicit-any'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const isIOS = /iPad|iPhone|iPod/.test(ua) && !(window as any)?.MSStream
-  // 'eslint-disable-next-line @typescript-eslint/no-explicit-any'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const isWebView = /(wv|WebView)/i.test(ua) || /KAKAOTALK|NAVER/i.test(ua)
 
   if (isIOS && navigator.share) {
